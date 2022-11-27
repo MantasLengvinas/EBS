ac736fa157aff57a9ea66a25082648f1a738c8cd
Mantas Lengvinas <mantaslengvin@gmail.com>
2022-11-27 18:39:54 +0200

    added profile and history pages

---
+++ EBSApp/Pages/ClientView/Profile.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EBSApp.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EBSApp.Pages.ClientView
+{
+    public partial class Profile : ComponentBase
+    {
+        [Inject]
+        UserStore UserStore { get; set; }
+    }
+}
+
